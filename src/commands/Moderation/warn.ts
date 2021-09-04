aef0318f88957e9b7aeeaa8e4a78e798f30cf6ef
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-04 11:44:52 +1000

    feat(moderation): set field and pardon warns

--- src/commands/Moderation/warn.ts
+++ src/commands/Moderation/warn.ts
@@ -6,8 +6,9 @@ import type { Args } from '@sapphire/framework'
 
 @ApplyOptions<ArielCommandOptions>({
   description: 'Warn a user',
-  usage: '<@user / User ID> [reason]',
-  subCommands: ['remove', { input: 'warn', default: true }]
+  detailedDescription: 'Warn a user, remove a warn, set a field (reason only) or pardon a user\'s warn.',
+  usage: '[remove | set | pardon] <@user / User ID> [reason]',
+  subCommands: ['remove', 'set', 'pardon', { input: 'warn', default: true }]
 })
 export default class Warn extends ArielCommand {
   @RequiresUserPermissions('KICK_MEMBERS')
@@ -35,6 +36,65 @@ export default class Warn extends ArielCommand {
     if (user.id === this.container.client.id) return await message.channel.send('I won\'t have any warns.')
 
     return await this.RemoveWarn(user, warnID, message)
+  }
+
+  @RequiresUserPermissions('KICK_MEMBERS')
+  public async set(message: Message, args: Args) {
+    const user = (await args.pickResult('member')).value.user
+    const warnID = (await args.pickResult('string')).value
+    const field = (await args.pickResult('string')).value
+    const newVal = (await args.restResult('string')).value
+
+    if (!warnID) return await message.channel.send('You must provide a warn ID. (Must be a valid one)')
+    if (!field) return await message.channel.send('Please specify a field. (hint: You can only set the `reason`)')
+    if (!newVal) return await message.channel.send('Please specify a new value for this field.')
+
+    if (user.id === message.author.id) return await message.channel.send('You cannot set warn fields for yourself.')
+    if (user.id === this.container.client.id) return await message.channel.send('I won\'t have any warns.')
+
+    return await this.SetWarn(message, user ?? message.author, warnID, field, newVal)
+  }
+
+  @RequiresUserPermissions('KICK_MEMBERS')
+  public async pardon(message: Message, args: Args) {
+    const { user } = (await args.pickResult('member')).value
+    const warnID = (await args.pickResult('string')).value
+
+    if (!user.id) {
+      return await message.channel.send('You must provide a user by mentioning them or by providing their user ID.')
+    }
+    if (!warnID) return await message.channel.send('You must provide a warn ID. (Must be a valid one)')
+
+    return await this.Pardon(message, user, warnID)
+  }
+
+  private async Pardon(message: Message, user: User, ID: string) {
+    const isPardoned = (await Warnings.findOne({ user: user.id, id: ID, guild: message.guild.id })).pardoned
+
+    await Warnings.findOneAndUpdate(
+      { user: user.id, id: ID, guild: message.guild.id },
+      { $set: { pardoned: !isPardoned } }
+    )
+
+    return await message.channel.send(
+      isPardoned ? `${user.toString()}'s case is no longer pardoned.` : `${user.toString()}'s case has been pardoned.`
+    )
+  }
+
+  private async SetWarn(message: Message, user: User, ID: string, field?: string, newVal?: string) {
+    switch (field) {
+      case 'reason': {
+        await Warnings.findOneAndUpdate({ user: user.id, id: ID, guild: message.guild.id }, { reason: newVal })
+
+        return await message.channel.send(`Successfully updated the reason to \`${newVal}\``)
+      }
+
+      default: {
+        return await message.channel.send(
+          'You can only set the **reason**. Also your command should look like: `warn @user warnID reason new value here`'
+        )
+      }
+    }
   }
 
   private async RemoveWarn(user: User, ID: string, message: Message) {
