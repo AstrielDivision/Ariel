82c5010429a0e7ea33dacd2a1ddeaa85f6c5a85d
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-04 10:59:49 +1000

    feat(moderation): added warns command

---
+++ src/commands/Moderation/warns.ts
@@ -0,0 +1,60 @@
+import { ArielCommand, ArielCommandOptions } from '#lib/Structures/BaseCommand'
+import { Message, User, MessageEmbed } from 'discord.js'
+import { ApplyOptions } from '@sapphire/decorators'
+import type { Args } from '@sapphire/framework'
+import Warnings from '#lib/Models/Warnings'
+
+@ApplyOptions<ArielCommandOptions>({
+  description: 'Fetch your warnings or another users warnings',
+  usage: '<@user / userID> [warnID]'
+})
+export default class Warns extends ArielCommand {
+  public async run(message: Message, args: Args) {
+    const { user } = args.finished ? message.member : await args.pick('member')
+    const ID = await args.pick('string').catch(() => '')
+
+    const embed = new MessageEmbed()
+
+    if (ID) {
+      const warn = await this.FetchWarn(user, message.guild.id, ID)
+
+      if (!warn) return await message.channel.send('No warning with that ID exists.')
+
+      const moderator = await this.container.client.util.findUser(warn.mod)
+
+      embed.setTitle(`${user.id === message.author.id ? 'Your' : `${user.username}'s`} Warn | `)
+
+      embed.addField('Moderator', moderator.username, true)
+      embed.addField('Reason', warn.reason, true)
+      embed.setFooter(`Warn ID: ${warn.id}`)
+
+      return await message.channel.send({ embeds: [embed] })
+    }
+
+    const { warnings, count } = await this.FetchWarnings(user, message.guild.id)
+    embed.setTitle(`${user.id === message.author.id ? 'Your' : `${user.username}'s`} Warnings [${count ?? 0}]`)
+
+    embed.addField(
+      'Warnings',
+      warnings ? warnings.map(w => `\`${w.id}\``).join(', ') : 'This user doesn\'t have any warns.',
+      true
+    )
+
+    return await message.channel.send({ embeds: [embed] })
+  }
+
+  private async FetchWarn(user: User, guild: string, id: string) {
+    const warn = await Warnings.findOne({ user: user.id, guild, id })
+
+    return warn
+  }
+
+  private async FetchWarnings(user: User, guild: string) {
+    const warns = await Warnings.find({ user: user.id, guild })
+
+    return {
+      warnings: warns,
+      count: warns.length
+    }
+  }
+}
