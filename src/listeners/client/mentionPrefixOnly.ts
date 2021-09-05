525c950e841114c08537591f186e7f1fd56e7adb
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-05 10:14:04 +1000

    refactor(listeners): i forgot I had it like this

--- src/listeners/client/mentionPrefixOnly.ts
+++ src/listeners/client/mentionPrefixOnly.ts
@@ -1,13 +1,14 @@
 import { Events, Listener, ListenerOptions } from '@sapphire/framework'
 import { ApplyOptions } from '@sapphire/decorators'
 import type { Message } from 'discord.js'
-import cfg from '../../config'
+import GuildSettings from '#lib/Models/GuildSettings'
 
 @ApplyOptions<ListenerOptions>({
   event: Events.MentionPrefixOnly
 })
 export default class mentionPrefixOnly extends Listener {
   public async run(message: Message) {
-    return await message.channel.send(`This guilds current prefix is: ${cfg.prefix}`)
+    const { prefix } = await GuildSettings.findOne({ guild_id: message.guild.id })
+    return await message.channel.send(`This guilds current prefix is: ${prefix}`)
   }
 }
