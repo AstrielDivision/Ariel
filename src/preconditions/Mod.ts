2723c8f0168929aca300b4b16ffb609116c95ae1
Universal Studio <uni@un1.pw>
2021-09-05 05:23:33 +0200

    fix(mod-precondition): properly check permissions

--- src/preconditions/Mod.ts
+++ src/preconditions/Mod.ts
@@ -1,11 +1,29 @@
 import { Precondition, PreconditionResult } from '@sapphire/framework'
 import type { Message } from 'discord.js'
+import { Permissions } from 'discord.js'
+
+const authorizingPermissions = [
+  Permissions.FLAGS.KICK_MEMBERS,
+  Permissions.FLAGS.BAN_MEMBERS,
+  Permissions.FLAGS.MANAGE_GUILD,
+  Permissions.FLAGS.MANAGE_CHANNELS
+]
 
 export default class Mod extends Precondition {
   public run(message: Message): PreconditionResult {
     if (!message.guild) return this.error({ message: 'You cannot run this command in DMs.' })
-    return message.member.permissions.has('KICK_MEMBERS' || 'BAN_MEMBERS' || 'MANAGE_GUILD' || 'MANAGE_CHANNELS')
-      ? this.ok()
-      : this.error({ message: 'You aren\'t allowed to execute this command.' })
+
+    let isAuthorized = false
+
+    for (const perm of authorizingPermissions) {
+      if (message.member.permissions.has(perm)) {
+        isAuthorized = true
+        break
+      }
+    }
+
+    if (isAuthorized) return this.ok()
+
+    return this.error({ message: 'You aren\'t allowed to execute this command.' })
   }
 }
