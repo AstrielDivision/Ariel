36a83866d012fd9d55a522c59296256f03f24238
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-05 18:19:39 +1000

    refactor(moderation): resolve warning via ID instead of mentioning

--- src/commands/Moderation/reason.ts
+++ src/commands/Moderation/reason.ts
@@ -6,27 +6,26 @@ import Warnings from '#lib/Models/Warnings'
 
 @ApplyOptions<ArielCommandOptions>({
   description: 'Set a warning reason',
-  usage: '<@user / ID> <WarningID> <reason>'
+  usage: '<WarningID> <reason>'
 })
 export default class Reason extends ArielCommand {
   @RequiresUserPermissions('KICK_MEMBERS')
   public async run(message: Message, args: Args) {
-    const user = (await args.pickResult('member')).value.user
     const warnID = (await args.pickResult('string')).value
     const newVal = (await args.restResult('string')).value
 
     if (!warnID) return await message.channel.send('You must provide a warn ID. (Must be a valid one)')
     if (!newVal) return await message.channel.send('Please specify a new value for this field.')
 
-    if (!message.member.permissions.has('ADMINISTRATOR') && user.id === message.author.id) {
-      return await message.channel.send('You cannot set warning reasons for yourself.')
-    }
-    if (user.id === this.container.client.id) return await message.channel.send('I won\'t have any warns.')
+    const warning = await Warnings.findOne({ id: warnID, guild: message.guild.id })
 
-    await Warnings.findOneAndUpdate(
-      { user: user.id, id: warnID, guild: message.guild.id },
-      { $set: { reason: newVal } }
-    )
+    if (!warning) return await message.channel.send('Could\'t find a warning with that ID.')
+
+    if (!message.member.permissions.has('ADMINISTRATOR') && warning.user === message.author.id) {
+      return await message.channel.send('You cannot set warn reasons for yourself.')
+    }
+
+    await warning.updateOne({ $set: { reason: newVal } })
 
     return await message.channel.send(`Successfully updated the reason to \`${newVal}\``)
   }
