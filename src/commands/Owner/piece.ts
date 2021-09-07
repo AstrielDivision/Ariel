a7877b95115169e321d5ad1de30c8126222201ef
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-07 20:22:00 +1000

    refactor(owner): update piece error response

--- src/commands/Owner/piece.ts
+++ src/commands/Owner/piece.ts
@@ -24,24 +24,23 @@ export default class Piece extends ArielCommand {
   public async disable(message: Message, args: Args) {
     const { success, value } = await args.pickResult('piece')
 
-    if (!success) return await message.channel.send('Could not resolve that name to a Piece')
+    if (!success) return await message.channel.send('Could not resolve Piece name')
 
     const piece = value
 
     piece.enabled = false
 
-    return await message.channel.send(`Disabled ${piece.name}`)
+    return await message.channel.send(`Disabled \`${piece.name}\``)
   }
 
   public async $reload(message: Message, args: Args) {
     const { success, value } = await args.pickResult('piece')
 
-    if (!success) return await message.channel.send('Could not resolve that name to a Piece')
-
+    if (!success) return await message.channel.send('Could not resolve Piece name')
     const piece = value
 
     await piece.reload()
 
-    return await message.channel.send(`Reloaded ${piece.name}`)
+    return await message.channel.send(`Reloaded \`${piece.name}\``)
   }
 }
