7917889a78c84e3efa3f59618ba28af78c48e508
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-09 09:35:11 +1000

    refactor(configuration): move code around

--- src/commands/Configuration/anti.ts
+++ src/commands/Configuration/anti.ts
@@ -17,35 +17,41 @@ export default class Settings extends ArielCommand {
 
   @RequiresUserPermissions('MANAGE_GUILD')
   public async enable(message: Message, args: Args) {
-    const setting = (await args.pickResult('string')).value
+    const setting = args.finished ? null : await args.pick('string')
 
     if (!setting) return await this.defaultEmbed(message)
 
     switch (setting.toLowerCase()) {
       case 'unmentionable': {
         if (!message.guild.me.permissions.has('MANAGE_NICKNAMES')) {
-          return await message.channel.send(i18.t('commands/anti:permissionErr', { keys: { perm: 'MANGE_NICKNAMES' } }))
+          return await message.channel.send(i18.t('commands/anti:permissionErr', { perm: 'MANGE_NICKNAMES' }))
         }
 
-        return await this.EnableAnti(message, 'unmentionable')
+        await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { 'anti.unmentionable': true } })
+
+        return await message.channel.send(i18.t('commands/anti:enabled', { enabled: 'unmentionable names' }))
       }
 
       case 'invite':
       case 'invites': {
         if (!message.guild.me.permissions.has('MANAGE_MESSAGES')) {
-          return await message.channel.send(i18.t('commands/anti:permissionErr', { keys: { perm: 'MANGE_MESSAGES' } }))
+          return await message.channel.send(i18.t('commands/anti:permissionErr', { perm: 'MANGE_MESSAGES' }))
         }
 
-        return await this.EnableAnti(message, 'invites')
+        await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { 'anti.invites': true } })
+
+        return await message.channel.send(i18.t('commands/anti:enabled', { enabled: 'discord invites' }))
       }
 
       case 'gift':
       case 'gifts': {
         if (!message.guild.me.permissions.has('MANAGE_MESSAGES')) {
-          return await message.channel.send(i18.t('commands/anti:permissionErr', { keys: { perm: 'MANGE_MESSAGES' } }))
+          return await message.channel.send(i18.t('commands/anti:permissionErr', { perm: 'MANGE_MESSAGES' }))
         }
 
-        return await this.EnableAnti(message, 'gifts')
+        await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { 'anti.gifts': true } })
+
+        return await message.channel.send(i18.t('commands/anti:enabled', { enabled: 'discord gifts' }))
       }
 
       default: {
@@ -56,63 +62,17 @@ export default class Settings extends ArielCommand {
 
   @RequiresUserPermissions('MANAGE_GUILD')
   public async disable(message: Message, args: Args) {
-    const setting = (await args.pickResult('string')).value
+    const setting = args.finished ? null : await args.pick('string')
 
     if (!setting) return await this.defaultEmbed(message)
 
     switch (setting.toLowerCase()) {
       case 'unmentionable': {
-        return await this.DisableAnti(message, 'unmentionable')
-      }
-
-      case 'invite':
-      case 'invites': {
-        return await this.DisableAnti(message, 'invites')
-      }
-
-      case 'gift':
-      case 'gifts': {
-        return await this.DisableAnti(message, 'gifts')
-      }
-
-      default: {
-        return await this.defaultEmbed(message)
-      }
-    }
-  }
-
-  private async EnableAnti(message: Message, anti: string) {
-    switch (anti) {
-      case 'unmentionable': {
-        await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { 'anti.unmentionable': true } })
-
-        return await message.channel.send(i18.t('commands/anti:enabled', { keys: { name: 'unmentionable names' } }))
-      }
-      case 'invite':
-      case 'invites': {
-        await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { 'anti.invites': true } })
-
-        return await message.channel.send(i18.t('commands/anti:enabled', { keys: { name: 'discord invites' } }))
-      }
-
-      case 'gift':
-      case 'gifts': {
-        await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { 'anti.gifts': true } })
-
-        return await message.channel.send(i18.t('commands/anti:enabled', { keys: { name: 'discord gifts' } }))
-      }
-    }
-
-    return undefined
-  }
-
-  private async DisableAnti(message: Message, anti: string) {
-    switch (anti) {
-      case 'unmentionable': {
         await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { 'anti.unmentionable': false } })
 
         return await message.channel.send(i18.t('commands/anti:disabled', { disabled: 'unmentionable names' }))
       }
+
       case 'invite':
       case 'invites': {
         await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { 'anti.invites': false } })
@@ -126,9 +86,11 @@ export default class Settings extends ArielCommand {
 
         return await message.channel.send(i18.t('commands/anti:disabled', { disabled: 'discord gifts' }))
       }
-    }
 
-    return undefined
+      default: {
+        return await this.defaultEmbed(message)
+      }
+    }
   }
 
   private async defaultEmbed(message: Message) {
@@ -138,8 +100,8 @@ export default class Settings extends ArielCommand {
       .setTitle(`${i18.t('commands/anti:title')} | ${message.guild.name}`)
       .setDescription(
         `${i18.t('commands/anti:unmentionable', { yesNo: anti.unmentionable ? 'Yes' : 'No' })}\n` +
-          `${i18.t('commands/anti:message', { name: 'invites', yesNo: anti.invites ? 'Yes' : 'No' })}\n` +
-          `${i18.t('commands/anti:message', { name: 'gifts', yesNo: anti.gifts ? 'Yes' : 'No' })}`
+          `${i18.t('commands/anti:filtering', { name: 'invites', yesNo: anti.invites ? 'Yes' : 'No' })}\n` +
+          `${i18.t('commands/anti:filtering', { name: 'gifts', yesNo: anti.gifts ? 'Yes' : 'No' })}`
       )
       .setFooter(i18.t('commands/anti:disableFooter', { prefix }))
 
