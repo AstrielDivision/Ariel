f10570b1a6a905c10bf9a3766efa800944d3a3f6
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-18 11:46:02 +1000

    refactor(setup): remove coloretteOptions

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,8 +1,6 @@
 import '@sapphire/plugin-api/register'
 import '@sapphire/plugin-i18next/register'
-import { options as coloretteOptions } from 'colorette'
 import 'reflect-metadata'
 import { inspect } from 'util'
 
 inspect.defaultOptions.depth = 1
-coloretteOptions.enabled = true
