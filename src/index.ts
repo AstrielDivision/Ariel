771a95617662a50c5c14782e3cec325985a07e67
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-09-20 20:11:48 +1000

    refactor: fix formatting

--- src/index.ts
+++ src/index.ts
@@ -12,26 +12,34 @@ const client = new Client({
   caseInsensitiveCommands: true,
   logger: { instance: new Logger('Ariel') },
   intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_BANS', 'GUILD_WEBHOOKS', 'GUILD_MEMBERS', 'GUILD_MESSAGE_REACTIONS'],
-  fetchLanguage: async (message: InternationalizationContext) => {
-    const { language } = await GuildSettings.findOne({ guild_id: message.guild.id })
-
-    return language
-  },
   i18n: {
-    i18next: {
+    fetchLanguage: async (message: InternationalizationContext) => {
+      const { language } = await GuildSettings.findOne({ guild_id: message.guild.id })
+
+      return language
+    },
+    i18next: (_: string[], languages: string[]) => ({
+      supportLngs: languages,
+      preload: languages,
+      load: 'all',
+      initImmediate: false,
+      fallbackLng: 'en-US',
+      returnObjects: true,
+      returnEmptyString: false,
+      returnNull: false,
       interpolation: {
+        escapeValue: false,
         format: (...[value, format]: Parameters<FormatFunction>) => {
           switch (format) {
             case 'permissions': {
               return (value as string[]).map(v => `\`${v}\``).join(', ')
             }
-            default: {
-              return null
-            }
+            default:
+              return value as string
           }
         }
       }
-    }
+    })
   },
   api: {
     listenOptions: {
