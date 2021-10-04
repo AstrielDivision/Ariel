b3f3d72a1f70ecea8a97dd61cdd6c2df98d4a893
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-10-04 21:40:10 +1100

    refactor(cryptography): use args.finished

--- src/commands/Cryptography/rabbit.ts
+++ src/commands/Cryptography/rabbit.ts
@@ -12,7 +12,7 @@ import { Message, Permissions } from 'discord.js'
 export default class Rabbit extends ArielCommand {
   public async run(message: Message, args: ArielCommand.Args) {
     const decryptFlag = args.getFlags('d', 'decrypt')
-    const text = (await args.restResult('string')).value
+    const text = args.finished ? null : await args.rest('string')
     const secret = args.getOption('s', 'secret')
 
     if (!text) return await message.channel.send(args.t('commands/cryptography:noText'))
