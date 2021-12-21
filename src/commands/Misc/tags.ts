b072d282c2b05c0c6ba160fb934e9318789b2c83
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-12-21 19:05:03 +1100

    chore(models): rename tagcommand

--- src/commands/Misc/tags.ts
+++ src/commands/Misc/tags.ts
@@ -1,16 +1,21 @@
-import TagCommand from '#lib/Models/TagCommand'
+import Tag from '#lib/Models/Tags'
 import { ArielCommand, ArielCommandOptions } from '#lib/Structures/Command'
 import { ApplyOptions, RequiresUserPermissions } from '@sapphire/decorators'
+import { codeBlock } from '@sapphire/utilities'
 import { Message, MessageEmbed } from 'discord.js'
 
 @ApplyOptions<ArielCommandOptions>({
-  description: 'Solve this',
-  usage: '[show | create | delete] <required>',
-  subCommands: ['create', 'delete', { input: 'show', default: true }]
+  aliases: ['tag'],
+  description: 'A custom command that stores text',
+  detailedDescription: 'A tag is a custom command that stores text, when executed it returns with the text stored',
+  usage: '[show | create | delete] <name> <data>',
+  flags: ['embed'],
+  options: ['color'],
+  subCommands: ['create', 'delete', 'edit', 'source', { input: 'show', default: true }]
 })
 export default class Tags extends ArielCommand {
   public async show(message: Message) {
-    const tags = await TagCommand.find({ guild_id: message.guild.id })
+    const tags = await Tag.find({ guild_id: message.guild.id })
 
     const parsed = tags.map(t => t.name).join('\n')
 
@@ -23,14 +28,18 @@ export default class Tags extends ArielCommand {
   public async create(message: Message, args: ArielCommand.Args) {
     const name = (await args.pickResult('string')).value
     const data = (await args.restResult('string')).value
+    const embed = args.getFlags('embed')
+    const color = args.getOption('color')
 
     if (!name) return await message.channel.send('You must provide a name')
     if (!data) return await message.channel.send('You must provide data (Like a message)')
 
-    await new TagCommand({
+    await new Tag({
       guild_id: message.guild.id,
       name: name.toLowerCase(),
-      data
+      data,
+      embed,
+      color
     }).save()
 
     return await message.channel.send(`Successfully add the tag \`${name}\``)
@@ -43,11 +52,37 @@ export default class Tags extends ArielCommand {
     if (!name) return await message.channel.send('You must provide a name')
 
     try {
-      await TagCommand.findOneAndRemove({ name, guild_id: message.guild.id })
+      await Tag.findOneAndRemove({ name, guild_id: message.guild.id })
     } catch (err) {
       return await message.channel.send(`Could not find tag \`${name}\``)
     }
 
     return await message.channel.send(`Deleted \`${name}\``)
+  }
+
+  @RequiresUserPermissions('MANAGE_MESSAGES')
+  public async edit(message: Message, args: ArielCommand.Args) {
+    const name = (await args.pickResult('string')).value
+    const data = (await args.restResult('string')).value
+    const embed = args.getFlags('embed')
+    const color = args.getOption('color')
+
+    if (!name) return await message.channel.send('You must provide a name of a tag')
+    if (!data) return await message.channel.send('You must provide data to replace the old data')
+
+    await Tag.findOneAndUpdate({ guild_id: message.guild.id, name }, { data, embed, color })
+
+    return await message.channel.send(`Successfully updated \`${name}\``)
+  }
+
+  @RequiresUserPermissions('MANAGE_MESSAGES')
+  public async source(message: Message, args: ArielCommand.Args) {
+    const name = (await args.pickResult('string')).value
+
+    if (!name) return await message.channel.send('You must provide a name of a tag')
+
+    const { data } = await Tag.findOne({ guild_id: message.guild.id, name })
+
+    return await message.channel.send(codeBlock('md', data))
   }
 }
--- src/lib/Models/Tags.ts
+++ src/lib/Models/Tags.ts
@@ -1,11 +1,13 @@
-import type { TagCommand } from '#types'
+import type { Tag } from '#types'
 import { model, Schema } from 'mongoose'
 
 const schema = new Schema({
   registeredAt: { type: String, required: false, default: Date.now().toString() },
   guild_id: { type: String, required: true },
   name: { type: String, required: true },
-  data: { type: String, required: true }
+  data: { type: String, required: true },
+  color: { type: String, required: false },
+  embed: { type: Boolean, required: false, default: false }
 })
 
-export default model<TagCommand>('Tags', schema)
+export default model<Tag>('Tags', schema)
