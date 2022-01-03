14da7deb46708bd744ed4e71de8c6e2c1ded184e
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-01-03 22:02:54 +1100

    fix(configuration): use arg.t instead of resolveKey

--- src/commands/Configuration/prefix.ts
+++ src/commands/Configuration/prefix.ts
@@ -2,7 +2,6 @@ import { envParseString } from '#lib/env/parser'
 import GuildSettings from '#lib/Models/GuildSettings'
 import { ArielCommand, ArielCommandOptions } from '#lib/Structures/Command'
 import { ApplyOptions, RequiresUserPermissions } from '@sapphire/decorators'
-import { resolveKey } from '@sapphire/plugin-i18next'
 import { Message } from 'discord.js'
 
 @ApplyOptions<ArielCommandOptions>({
@@ -15,26 +14,26 @@ export default class Prefix extends ArielCommand {
   public async set(message: Message, args: ArielCommand.Args) {
     const prefix = (await args.pickResult('string')).value
 
-    if (!prefix) return await message.channel.send(await resolveKey(message, 'commands/config:prefix.noPrefix'))
+    if (!prefix) return await message.channel.send(await args.t('commands/config:prefix.noPrefix'))
     if (prefix.length > 3) {
-      return await message.channel.send(await resolveKey(message, 'commands/config:prefix.prefixToLong'))
+      return await message.channel.send(await args.t('commands/config:prefix.prefixToLong'))
     }
 
     await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { prefix: prefix } })
 
-    return await message.channel.send(await resolveKey(message, 'commands/config:prefix.setPrefix', { prefix }))
+    return await message.channel.send(await args.t('commands/config:prefix.setPrefix', { prefix }))
   }
 
   @RequiresUserPermissions('MANAGE_GUILD')
-  public async reset(message: Message) {
+  public async reset(message: Message, args: ArielCommand.Args) {
     await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id }, { $set: { prefix: envParseString('PREFIX') } })
 
-    return await message.channel.send(await resolveKey(message, 'commands/config:prefix.success.resetPrefix'))
+    return await message.channel.send(await args.t('commands/config:prefix.success.resetPrefix'))
   }
 
-  public async show(message: Message) {
+  public async show(message: Message, args: ArielCommand.Args) {
     const { prefix } = await GuildSettings.findOneAndUpdate({ guild_id: message.guild.id })
 
-    return await message.channel.send(await resolveKey(message, 'commands/config:prefix.showPrefix', { prefix }))
+    return await message.channel.send(await args.t('commands/config:prefix.showPrefix', { prefix }))
   }
 }
