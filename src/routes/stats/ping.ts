0b613c48ec16b60e599c9035abebb876fff7af19
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-02-05 21:46:04 +1100

    chore: remove some files
    Some of these files will be rewritten

--- src/routes/stats/ping.ts
+++
@@ -1,9 +0,0 @@
-import { ApplyOptions } from '@sapphire/decorators'
-import { ApiRequest, ApiResponse, methods, Route, RouteOptions } from '@sapphire/plugin-api'
-
-@ApplyOptions<RouteOptions>({ route: 'members' })
-export class UserRoute extends Route {
-  public [methods.GET](_request: ApiRequest, response: ApiResponse) {
-    return response.json({ ping: this.container.client.ws.ping })
-  }
-}
